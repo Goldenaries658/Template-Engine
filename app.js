6ae3bfc65d9e1eeff45fab258cd7988644b55cfc
Goldenaries658 <59972622+Goldenaries658@users.noreply.github.com>
2020-05-24 03:37:05 +0100

    Finalised createTeam function

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const inquirer = require('inquirer');
 const path = require('path');
 const fs = require('fs');
 const colors = require('colors');
+const header = require('./lib/Header');
 
 const OUTPUT_DIR = path.resolve(__dirname, 'output');
 const outputPath = path.join(OUTPUT_DIR, 'team.html');
@@ -88,23 +89,35 @@ const createEmployee = async (role) => {
 };
 
 const createTeam = async () => {
-  console.log('Welcome to the Template Engine!'.blue.bold);
-  console.log('First, enter in manager details:'.green);
+  // Function to display employee details
+  const displayInfo = async (employee) => {
+    console.log(
+      '---------------------------------------------------------------'.bold
+        .green
+    );
+    console.log(employee.getRole().blue.bold);
+    console.log('Name: '.bold + employee.name.blue.bold);
+    console.log('ID: '.bold + employee.id.blue.bold);
+    console.log('Email: '.bold + employee.email.blue.bold);
+    if ('officeNumber' in employee) {
+      console.log('Office Number: '.bold + employee.officeNumber.blue.bold);
+    } else if ('github' in employee) {
+      console.log('GitHub: '.bold + employee.github.blue.bold);
+    } else {
+      console.log('School: '.bold + employee.school.blue.bold);
+    }
+    console.log(
+      '---------------------------------------------------------------'.bold
+        .green
+    );
+  };
 
   // Function to confirm user input
   const confirmSelection = async (employee) => {
-    console.log('-------------------------'.bold.green);
-    console.log('Name: '.bold + employee.name.blue);
-    console.log('ID: '.bold + employee.id.blue);
-    console.log('Email: '.bold + employee.email.blue);
-    if ('officeNumber' in employee) {
-      console.log('Office Number: '.bold + employee.officeNumber.blue);
-    } else if ('gitHub' in employee) {
-      console.log('GitHub: '.bold + employee.gitHub.blue);
-    } else {
-      console.log('School: '.bold + employee.school.blue);
-    }
-    console.log('-------------------------'.bold.green);
+    console.clear();
+    console.log(header);
+    // Displaying selected info
+    displayInfo(employee);
     const selection = await inquirer.prompt({
       type: 'list',
       name: 'selection',
@@ -115,17 +128,69 @@ const createTeam = async () => {
     if (selection.selection === 'Yes') {
       return true;
     } else {
+      console.clear();
+      console.log('Re-enter details:'.magenta.bold);
       return false;
     }
   };
 
-  let managerChosen = false;
-  while (!managerChosen) {
-    const manager = await createEmployee('manager');
-    managerChosen = await confirmSelection(manager);
+  // Checking for extra employees
+  const confirmEmployees = async (employeeList) => {
+    console.clear();
+    console.log(header);
+    employeeList.forEach((employee) => {
+      displayInfo(employee);
+    });
+    const selection = await inquirer.prompt({
+      type: 'list',
+      name: 'selection',
+      message: 'Are you done adding employees?'.red,
+      choices: ['Yes', 'No'],
+    });
 
+    if (selection.selection === 'Yes') {
+      console.log('true');
+
+      return true;
+    } else {
+      console.log('false');
+      return false;
+    }
+  };
+
+  const teamArray = [];
+  // Entering manager details
+  console.clear();
+  console.log(header);
+  console.log('First, enter in manager details:'.magenta.bold);
+  let managerChosen = false;
+  let manager;
+  while (!managerChosen) {
+    manager = await createEmployee('manager');
+    managerChosen = await confirmSelection(manager);
   }
-  console.log('Next add a team member:'.green);
+  // Pushing confirmed manager to team array
+  teamArray.push(manager);
+
+  // Entering employee details
+  console.clear();
+  console.log(header);
+  console.log('Next add a team member:'.magenta.bold);
+
+  // Nested while loop allows infinite number of employees to be entered whilst still
+  // checking for validity
+  let employeeListComplete = false;
+  while (!employeeListComplete) {
+    let employeeChosen = false;
+    while (!employeeChosen) {
+      const employee = await createEmployee();
+      employeeChosen = await confirmSelection(employee);
+    }
+    // Pushing confirmed employee to team array
+    teamArray.push(employee);
+    // Confirming whether list is complete
+    employeeListComplete = await confirmEmployees(teamArray);
+  }
 };
 
 createTeam();
